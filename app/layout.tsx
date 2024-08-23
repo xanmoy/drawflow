868e66082ef167c21383312759a1eb502f240458
Tanmoy Ganguly <contact@xanmoy.me>
2024-08-24 02:06:38 +0530

    convex setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          {children}
-      </body>
+        <ConvexClientProvider>{children}</ConvexClientProvider>      </body>
     </html>
   );
 }
