9d003411e9cb26406031eab72a809c8134beb500
Tanmoy Ganguly <contact@xanmoy.me>
2024-08-24 18:12:42 +0530

    updated

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,9 +1,22 @@
-// 'use Client'
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import React from 'react'
+"use client"
+import { LogoutLink,  } from "@kinde-oss/kinde-auth-nextjs/components";
+import React, { useEffect } from 'react'
 import { Button } from '@/components/ui/button'
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+
 
 function Dashboard() {
+  const {user}:any = useKindeBrowserClient() 
+
+  const getUser=useQuery(api.user.getUser,{email:user?.email})
+
+  useEffect(() => {
+    if (user) {
+      console.log(getUser)
+    }
+  },[user])
   return (
     <div>
       <LogoutLink>
