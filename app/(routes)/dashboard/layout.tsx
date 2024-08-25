1fdf0485660599c650ae78ff5b2ab875735a59b3
Tanmoy Ganguly <contact@xanmoy.me>
2024-08-26 02:51:29 +0530

    updated

---
+++ app/(routes)/dashboard/layout.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { api } from "@/convex/_generated/api";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useConvex } from "convex/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+function DashboardLayout(
+    {
+        children,
+    }: Readonly<{
+        children: React.ReactNode;
+    }>
+) {
+    const convex = useConvex();
+    const { user }: any = useKindeBrowserClient();
+    
+    const router = useRouter();
+    useEffect(() => {
+        user && checkTeam();
+    }, [user])
+
+    const checkTeam = async () => {
+        const result = await convex.query(api.teams.getTeam,
+            { email: user?.email });
+
+        if (!result?.length) {
+            router.push('teams/create')
+        }
+    }
+
+    return (
+        
+           
+              
+                    <div className='col-span-4 ml-72 bg-neutral-900 text-white'>
+                        {children}
+                    </div>
+                
+           
+    )
+}
+
+export default DashboardLayout
--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -2,21 +2,36 @@
 import { LogoutLink,  } from "@kinde-oss/kinde-auth-nextjs/components";
 import React, { useEffect } from 'react'
 import { Button } from '@/components/ui/button'
-import { useQuery } from "convex/react";
+import { useConvex, useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 
 function Dashboard() {
+  const convex = useConvex();
   const {user}:any = useKindeBrowserClient() 
+  // const getUser=useQuery(api.user.getUser,{email:user?.email})
 
-  const getUser=useQuery(api.user.getUser,{email:user?.email})
+  const createUser=useMutation(api.user.createUser)
 
   useEffect(() => {
     if (user) {
-      console.log(getUser)
+      checkUser()  
     }
-  },[user])
+  }, [user])
+  
+  const checkUser = async () => {
+    const result = await convex.query(api.user.getUser,{email:user?.email})
+    if (!result?.length) {
+      createUser({
+        name: user.given_name,
+        email: user.email,
+        image: user.picture
+      }).then((resp) => {
+        console.log(resp)
+      })
+    }
+  }
   return (
     <div>
       <LogoutLink>
